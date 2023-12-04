2e8e901039d66986ca0e46d4de718a61e41b4cef
Nick Toth <nick.toth@ifoodds.com>
2023-12-04 11:23:05 -0600

    Day 2 part 1

--- 2023/src/main/java/com/AdventOfCode2023/Day2.java
+++ 2023/src/main/java/com/AdventOfCode2023/Day2.java
@@ -5,14 +5,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
+import java.util.regex.Pattern;
 
-public class Day1 {
-    public int ExecutePart1(String file) throws IOException {
-        InputStream inputStream = null;
+public class Day2 {
+    public int ExecutePart1(String file, int red, int blue, int green) throws IOException {
         var result = 0;
         var lines = new ArrayList<String>();
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + file))) {
             while (br.ready()) {
                 var line = br.readLine();
                 lines.add(line);
@@ -20,14 +22,24 @@ public class Day1 {
         }
 
         for (int x = 0; x < lines.size(); x++) {
-            // Traverse the string
-            var stack = new Stack<Integer>();
-            for (int i = 0; i < lines.get(x).length(); i++) {
-                if (Character.isDigit(lines.get(x).charAt(i))) {
-                    stack.push(Character.getNumericValue(lines.get(x).charAt(i)));
+            Map<String, Integer> colorCounts = new HashMap<>();
+            var line = lines.get(x);
+            var pattern = Pattern.compile("(\\d+)\\s(\\w+)");
+            var matcher = pattern.matcher(line);
+            while(matcher.find()) {
+                var color = matcher.group(2);
+                var count = Integer.parseInt(matcher.group(1));
+                if (count > colorCounts.getOrDefault(color, 0)) {
+                    colorCounts.put(color, count);
                 }
             }
-            result += stack.firstElement() * 10 + stack.lastElement();
+            if (colorCounts.getOrDefault("red", 0) <= red && colorCounts.getOrDefault("blue", 0) <= blue && colorCounts.getOrDefault("green", 0) <= green) {
+                var gameIdPattern = Pattern.compile("Game (\\d+):");
+                var gameIdMatcher = gameIdPattern.matcher(line);
+                if (gameIdMatcher.find()){
+                    result += Integer.parseInt(gameIdMatcher.group(1));
+                }
+            }
         }
         return result;
     }
---
+++ 2023/src/test/java/com/AdventOfCode2023/Day2Tests.java
@@ -0,0 +1,22 @@
+package com.AdventOfCode2023;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
+class Day2Tests {
+	@Test
+	public void Test1() throws IOException {
+		var day2 = new Day2();
+		Assert.assertEquals(8, day2.ExecutePart1("/src/test/Inputs/Day2Example.txt", 12, 14, 13));
+	}
+
+	@Test
+	public void Part1() throws IOException {
+		var day2 = new Day2();
+		Assert.assertEquals(2265, day2.ExecutePart1("/src/test/Inputs/Day2Input.txt", 12, 14, 13));
+	}
+}
