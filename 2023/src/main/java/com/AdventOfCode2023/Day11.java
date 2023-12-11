6fe69e3bf3f0df4ad1f8e186db78f0105a71b4c2
Nick Toth <nick.toth@ifoodds.com>
2023-12-11 16:56:07 -0600

    part 2 day 11

--- 2023/src/main/java/com/AdventOfCode2023/Day11.java
+++ 2023/src/main/java/com/AdventOfCode2023/Day11.java
@@ -3,6 +3,7 @@ package com.AdventOfCode2023;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -46,7 +47,6 @@ public class Day11 {
         var lineLength = lines.get(0).length();
         var height = nodes.size() / lineLength;
         for(var x = 0; x < lineLength; x++) {
-//            for (var y = 0; y < height - 1; y++) { // Don't care about the last row expanding
                 int finalX = x;
                 var count = nodes.stream().filter(n -> n.X == finalX && n.Galaxy).count();
                 if (count == 0) {
@@ -58,7 +58,6 @@ public class Day11 {
                     x++;
                     ShiftRight(nodes, x, height);
                 }
-//            }
         }
 
         var galaxies = nodes.stream().filter(n -> n.Galaxy).toList();
@@ -76,14 +75,91 @@ public class Day11 {
         return result / 2; // Because I'm calculating 1 -> 9 and 9 -> 1 for example
     }
 
+    public BigInteger ExecutePart2(String file, int expansion) throws IOException {
+        var lines = new ArrayList<String>();
+        try (BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + file))) {
+            while (br.ready()) {
+                var line = br.readLine();
+                lines.add(line);
+            }
+        }
+
+        // Let's rethink this.
+        // Only store the galaxies.
+        var nodes = new HashSet<Node>();
+        var shiftUpCount = 0;
+        for(var y = 0; y < lines.size(); y++) {
+            var line = lines.get(y);
+            for(var x = 0; x < line.length(); x++) {
+                var c = line.charAt(x);
+                if (c == '#') {
+                    nodes.add(new Node(x, y + shiftUpCount));
+                }
+            }
+        }
+
+        var emptyRows = new ArrayList<Integer>();
+        var emptyColumns = new ArrayList<Integer>();
+
+        for (var x = 0; x < lines.get(0).length(); x++) {
+            int finalX = x;
+            var count = nodes.stream().filter(n -> n.X == finalX).count();
+            if (count == 0) {
+                emptyColumns.add(x);
+            }
+        }
+
+        var height = nodes.stream()
+                .map(Node::getY)
+                .max(Integer::compare);
+        for (var y = 0; y < height.get(); y++) {
+            int finalY = y;
+            var count = nodes.stream().filter(n -> n.Y == finalY).count();
+            if (count == 0) {
+                emptyRows.add(y);
+            }
+        }
+
+        var shift = 0;
+        for (var y: emptyRows) {
+            var finalShift = shift;
+            var nodesToShift = nodes.stream().filter(n -> n.Y > (y + finalShift)).toList();
+            for (var i = 0; i < nodesToShift.size(); i++) {
+                nodesToShift.get(i).Y += expansion;
+            }
+            shift += expansion;
+        }
+
+        shift = 0;
+        for (var x: emptyColumns) {
+            var finalShift = shift;
+            var nodesToShift = nodes.stream().filter(n -> n.X > (x + finalShift)).toList();
+            for (var i = 0; i < nodesToShift.size(); i++) {
+                nodesToShift.get(i).X += expansion;
+            }
+            shift += expansion;
+        }
+
+        var result = BigInteger.ZERO;
+        var nodesArray = nodes.toArray();
+        for (var g = 0; g < nodes.size(); g++) {
+            var galaxy1 = (Node) nodesArray[g];
+            for (var g2 = 0; g2 < nodes.size(); g2++) {
+                var galaxy2 = (Node) nodesArray[g2];
+                var distance = CalculateManhattanDistance(galaxy1.X, galaxy1.Y, galaxy2.X, galaxy2.Y);
+                result = result.add(BigInteger.valueOf(distance));
+            }
+        }
+//        Print(50, 50, nodes);
+        return result.divide(BigInteger.TWO); // Because I'm calculating 1 -> 9 and 9 -> 1 for example
+    }
     private void Print(int lineLength, int height, HashSet<Node> nodes) {
         for (var y = 0; y < height; y++) {
             for(var x = 0; x < lineLength; x++) {
                 int finalX = x;
                 int finalY = y;
-                var test = nodes.stream().filter(n2 -> n2.X == 0).toList();
-                var n = nodes.stream().filter(n2 -> n2.X == finalX && n2.Y == finalY).findFirst().get().Galaxy;
-                if (n) {
+                var n = nodes.stream().filter(n2 -> n2.X == finalX && n2.Y == finalY).findFirst();
+                if (!n.isEmpty()) {
                     System.out.print("#");
                 } else {
                     System.out.print(".");
@@ -95,7 +171,7 @@ public class Day11 {
     }
 
     private static int CalculateManhattanDistance(int x1, int y1, int x2, int y2) {
-        return Math.abs(x2 - x1) + Math.abs(y2 - y1);
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
     }
 
     public void ShiftRight(HashSet<Node> nodes, int x, int height) {
@@ -123,6 +199,15 @@ public class Day11 {
             X = x;
             Y = y;
             Galaxy = galaxy;
+        }
+        // Used for part 2
+        public Node(int x, int y) {
+            X = x;
+            Y = y;
+        }
+
+        public Integer getY() {
+            return Integer.valueOf(Y);
         }
     }
 
--- 2023/src/test/java/com/AdventOfCode2023/Day11Tests.java
+++ 2023/src/test/java/com/AdventOfCode2023/Day11Tests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.math.BigInteger;
 
 @SpringBootTest
 class Day11Tests {
@@ -18,5 +19,29 @@ class Day11Tests {
 	public void Part1() throws IOException {
 		var day11 = new Day11();
 		Assert.assertEquals(9918828, day11.ExecutePart1("/src/test/Inputs/Day11Input.txt"));
+	}
+
+	@Test
+	public void Test2() throws IOException {
+		var day11 = new Day11();
+		Assert.assertEquals((BigInteger.valueOf(374)), day11.ExecutePart2("/src/test/Inputs/Day11Example.txt", 1));
+	}
+
+	@Test
+	public void Test3() throws IOException {
+		var day11 = new Day11();
+		Assert.assertEquals((BigInteger.valueOf(1030)), day11.ExecutePart2("/src/test/Inputs/Day11Example.txt", 9));
+	}
+
+	@Test
+	public void Test4() throws IOException {
+		var day11 = new Day11();
+		Assert.assertEquals((BigInteger.valueOf(8410)), day11.ExecutePart2("/src/test/Inputs/Day11Example.txt", 99));
+	}
+
+	@Test
+	public void Part2() throws IOException {
+		var day11 = new Day11();
+		Assert.assertEquals(BigInteger.valueOf(Long.parseLong("692506533832")), day11.ExecutePart2("/src/test/Inputs/Day11Input.txt", 999999));
 	}
 }
